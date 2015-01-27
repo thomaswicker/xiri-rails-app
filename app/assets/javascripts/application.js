aa5f9f07b14515e24e4f05949eaf7ce45903c670
Thomas Wicker <design@thomaswicker.com>
2015-01-27 00:33:03 -0700

    responsive changes to SASS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,23 +35,6 @@ function clearEmail(){
  });
 
 
-//SMMOTHSCROLL JS
-
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
-
 // Masonry for Portfolio Section
 
 $(window).resize(function() {
